6475f66f87d824a796157e790e77bd0d1791bb36
Chase Newman <chasejn01@gmail.com>
2023-06-16 17:58:48 -0400

    add bootstrap

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ console.log("connected");
 
 fetch("https://data.sec.gov/submissions/CIK0001659815.json")
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        console.log(data);
+        console.log(data.name);
+        document.getElementById("company-name").innerHTML = data.name;
+    });
 
 
-fetch("https://data.sec.gov/api/xbrl/companyfacts/CIK0000320193.json")
-    .then(response => response.json())
-    .then(data => console.log(data))
