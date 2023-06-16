8d2f97c8df9245ee755af2d5d7735451ff671c71
Chase Newman <chasejn01@gmail.com>
2023-06-16 18:12:11 -0400

    add style.css and random number generator

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ fetch("https://data.sec.gov/submissions/CIK0001659815.json")
     .then(data => {
         console.log(data);
         console.log(data.name);
+        console.log(data.filings.recent.form[0]);
         document.getElementById("company-name").innerHTML = data.name;
+        document.getElementById("most-recent-filing").innerHTML = data.filings.recent.form[0];
     });
+
+console.log(Math.floor(Math.random() * 10))
 
 
